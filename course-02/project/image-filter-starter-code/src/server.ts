c174e895fb44d6594d97417827e938eeccb42537
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-10-14 18:33:09 +0300

    Deployable version ready

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import {Request, Response, Express} from 'express';
 
 (async () => {
 
   // Init the Express application
-  const app = express();
+  const app: Express = express();
 
   // Set the network port
-  const port = process.env.PORT || 8082;
+  const port: string | number = process.env.PORT || 8082;
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
@@ -28,12 +29,32 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+  app.get("/filteredimage",  async (req: Request, res: Response) => {
+      
+      const { image_url }: {image_url: string} = req.query
 
+      if (!image_url)
+        return res.status(400).send({message: "Image url is required as a query parameter"});
+      
+      try{
+        const url: string = await filterImageFromURL(image_url);
+        res.status(200).sendFile(url, (err: any) => {
+            if (err)
+            {
+              res.status(500).send({message: "Encountered error while sending file"});
+            }
+            deleteLocalFiles([url]);
+        });
+        
+      } catch(error){
+        res.status(422).send({message: "Error while processing Image", error: error});
+      }
+  });
   //! END @TODO1
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
